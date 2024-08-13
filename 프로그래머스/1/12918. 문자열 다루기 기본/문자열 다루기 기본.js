981b3590928dfdd5740979b6bb64611cc4bc0cf4
한승철 <101388379+HSCHEOL@users.noreply.github.com>
2024-08-13 10:31:42 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  if (s.length == 4 || s.length == 6) {
+    for (let i of s) {
+      if (isNaN(i)) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
