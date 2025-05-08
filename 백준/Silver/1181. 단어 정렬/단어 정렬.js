45c4e30aaf73141a3bbb6e264e50e3819629a129
HSCHEOL <knr0013@gmail.com>
2025-05-09 04:34:04 +0900

    [Silver V] Title: 단어 정렬, Time: 336 ms, Memory: 28668 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const [N, ...stdin] = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+
+
+//단어의 길이 값을 추출한다.
+//키를 영어 단어로 , 벨류를 영단어의 길이로 한다.
+//사전으로 정렬 후 -> 숫자 값으로 정렬
+
+const dictionary = new Map()
+
+for(i=0; i<N; i++){
+    dictionary.set(stdin[i] , stdin[i].length)
+}
+
+let sortDictionary = new Map([...dictionary].sort().sort((a,b) => a[1] - b[1]))
+sortDictionary = [...sortDictionary.keys()]
+
+console.log(sortDictionary.join('\n'))
+
+
